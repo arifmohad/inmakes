bc26048d9a2a63ab4bbc6b08530fb689656f3f03
arifmohad <100553781+arifmohad@users.noreply.github.com>
2023-09-15 10:18:12 +0530

    Delete travelproject/travel directory

--- travelproject/travel/travelapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import images,teams
-# Register your models here.
-
-
-admin.site.register(images)
-admin.site.register(teams)
--- travelproject/travel/travelapp/urls.py
+++
@@ -1,9 +0,0 @@
-from  . import views
-from django.urls import path, include
-
-urlpatterns = [
-    path('',views.demo,name='demo'),
-    path('call',views.additc,name='addict'),
-    path('addit/', views.result, name='additc'),
-    # path('result/', views.result, name='result'),
-]
